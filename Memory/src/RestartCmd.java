ffbd9d9d0de5cab57fcfdacb81b7dd43027c74a3
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-03-06 19:19:17 -0500

    Filled out the method stub found in the Restart.java class.

--- Memory/src/RestartCmd.java
+++ Memory/src/RestartCmd.java
@@ -18,10 +18,12 @@ public class RestartCmd extends Command {
 	public void executeClick(Drawing dwg) {
 		Deck deck = Deck.getUniqueInstance();
 		deck.shuffle();
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < deck.getSize(); i++) {
 			Card card = deck.deal();
-			dwg.replaceCard(i, card);
+			dwg.replaceCard(i, card); //Replace all the cards on the table
+			card.setInPlay(true); //Set each card to be in play
+			card.setFaceUp(false); //Set each card to be faced down
+			card.unsetHighlighted(); //Makes sure the card is not highlighted
 		}
-		dwg.newHighlight();
 	}
 }
